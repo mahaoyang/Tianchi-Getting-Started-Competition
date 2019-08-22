101e4e6ff36669a9334437799a46c19cf3478978
mahaoyang <mahydxygitlab>
2019-08-22 11:58:38 +0800

    init

--- deep_ctr.py
+++ deep_ctr.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
@@ -6,12 +7,36 @@ from sklearn.preprocessing import LabelEncoder
 from deepctr.models import DeepFM
 from deepctr.inputs import SparseFeat, get_fixlen_feature_names
 
-if __name__ == "__main__":
 
-    data = pd.read_csv("data/tianchi_fresh_comp_train_user.csv")
+def timestamp(t):
+    return int(time.mktime(time.strptime(t, "%Y-%m-%d %H")))
+
+
+def read(data):
+    # data['time'] = data['time'].apply(lambda x: timestamp(x), convert_dtype='int32')
+    data['time'] = int(time.time())
     sparse_features = ["user_id", "item_id", "item_category", "time"]
     target = ['behavior_type']
+    # 1.Label Encoding for sparse features,and do simple Transformation for dense features
+    for feat in sparse_features:
+        lbe = LabelEncoder()
+        data[feat] = lbe.fit_transform(data[feat])
+    # 2.count #unique features for each sparse field
+    fixlen_feature_columns = [SparseFeat(feat, data[feat].nunique())
+                              for feat in sparse_features]
+    linear_feature_columns = fixlen_feature_columns
+    dnn_feature_columns = fixlen_feature_columns
+    fixlen_feature_names = get_fixlen_feature_names(linear_feature_columns + dnn_feature_columns)
+    data_model_input = [data[name].values for name in fixlen_feature_names]
+    return data, data_model_input
 
+
+if __name__ == "__main__":
+
+    data = pd.read_csv("data/tianchi_fresh_comp_train_user.csv")[:100]
+    data['time'] = data['time'].apply(lambda x: timestamp(x), convert_dtype='int32')
+    sparse_features = ["user_id", "item_id", "item_category", "time"]
+    target = ['behavior_type']
     # 1.Label Encoding for sparse features,and do simple Transformation for dense features
     for feat in sparse_features:
         lbe = LabelEncoder()
@@ -33,7 +58,18 @@ if __name__ == "__main__":
 
     history = model.fit(train_model_input, train[target].values,
                         batch_size=256, epochs=10, verbose=2, validation_split=0.2, )
+
     pred_ans = model.predict(test_model_input, batch_size=256)
     print(pred_ans)
     print("test MSE", round(mean_squared_error(
         test[target].values, pred_ans), 4))
+
+    test = pd.read_csv('cf_predict.csv')
+    item = pd.read_csv('data/tianchi_fresh_comp_train_user.csv')
+    item = item[['item_id', 'item_category']].drop_duplicates('item_id').astype('int32')
+    # item = dict(zip(item['item_id'].values.tolist(), item['item_category'].values.tolist()))
+    test = test.join(item.set_index('item_id'), on='item_id', how='left')
+    test, test_model_input = read(test.astype('int32'))
+    pred_ans = model.predict(test_model_input, batch_size=256)
+    print(pred_ans)
+
